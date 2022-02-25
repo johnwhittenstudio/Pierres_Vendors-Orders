3bb80ffc87a515fc385aae04233d5e2172ceae45
John Whitten <johnwhitten.studio@gmail.com>
2022-02-25 14:57:41 -0800

    update github repo

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -21,6 +21,50 @@ namespace VendorOrder.Tests
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
+    [TestMethod]
+    public void OrderConstructor_CreatesAnInstanceOfAnOrder_True()
+    {
+      //Arrange
+      string title = "Sourdough";
+      string description = "5 loaves";
+      string price = "$15.00";
+      string date = "02/24/2022";
+      Order newOrder = new Order(title, description, price, date);
+
+      //Act
+      string instanceTitle = newOrder.Title;
+      string instanceDescription = newOrder.Description;
+      string instancePrice = newOrder.Price;
+      string instanceDate = newOrder.Date;
+
+      //Assert
+      Assert.AreEqual(title, instanceTitle);
+      Assert.AreEqual(description, instanceDescription);
+      Assert.AreEqual(price, instancePrice);
+      Assert.AreEqual(date, instanceDate);
+    }
+
+    public void OrderConstructor_CreatesAnInstanceOfAnOrder_False()
+    {
+      //Arrange
+      string title = "Bagels";
+      string description = "5 bagels";
+      string price = "$12.00";
+      string date = "02/24/2022";
+      Order newOrder = new Order(title, description, price, date);
+
+      //Act
+      string instanceTitle = newOrder.Title;
+      string instanceDescription = newOrder.Description;
+      string instancePrice = newOrder.Price;
+      string instanceDate = newOrder.Date;
+      
+      //Assert
+      Assert.AreEqual(title, instanceTitle);
+      Assert.AreEqual(description, instanceDescription);
+      Assert.AreEqual(price, instancePrice);
+      Assert.AreEqual(date, instanceDate);
+    }
 
   }
 }  
