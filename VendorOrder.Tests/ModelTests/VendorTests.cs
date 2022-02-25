e296ac318a575f802c50149d2c8e3f86541a9f64
John Whitten <johnwhitten.studio@gmail.com>
2022-02-25 15:52:46 -0800

    get name_returns name test passed

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -21,6 +21,19 @@ namespace VendorOrder.Tests
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      Vendor newVendor = new Vendor(name);
+
+      //Act
+      string result = newVendor.Name;
+
+      //Assert
+      Assert.AreEqual(name, result);
+    }
 
 
   }
