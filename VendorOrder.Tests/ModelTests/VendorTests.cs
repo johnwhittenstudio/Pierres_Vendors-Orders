6530fd8533d9c0532d48b5a0851776405ced0530
John Whitten <johnwhitten.studio@gmail.com>
2022-02-25 15:51:17 -0800

    vendor constructor_creates instance of vendor test passed

---
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendorOrder.Models;
+using System;
+
+namespace VendorOrder.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test Vendor");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+
+
+  }
+}
