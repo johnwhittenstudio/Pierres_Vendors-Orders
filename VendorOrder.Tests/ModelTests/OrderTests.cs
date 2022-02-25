5d6a1bc5b160ad8fba7b45023f2ef852041bcd09
John Whitten <johnwhitten.studio@gmail.com>
2022-02-25 15:30:08 -0800

    get all_return empty list test passed

--- VendorOrder.Tests/ModelTests/OrderTests.cs
+++ VendorOrder.Tests/ModelTests/OrderTests.cs
@@ -114,5 +114,19 @@ namespace VendorOrder.Tests
       Assert.AreEqual(updatedPrice, resultPrice);
     }
 
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      List<Order> newList = new List<Order> { };
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+
   }
 }  
