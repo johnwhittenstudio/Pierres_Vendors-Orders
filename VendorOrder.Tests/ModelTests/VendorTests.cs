23b93dd2982ca18089b1aaadb6fafe4d644eb092
John Whitten <johnwhitten.studio@gmail.com>
2022-02-25 16:03:44 -0800

    updated tests to include vendor description

--- VendorOrder.Tests/ModelTests/VendorTests.cs
+++ VendorOrder.Tests/ModelTests/VendorTests.cs
@@ -17,7 +17,7 @@ namespace VendorOrder.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor("test Vendor");
+      Vendor newVendor = new Vendor("test Vendor", "test Description");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
 
@@ -26,7 +26,8 @@ namespace VendorOrder.Tests
     {
       //Arrange
       string name = "Test Vendor";
-      Vendor newVendor = new Vendor(name);
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
 
       //Act
       string result = newVendor.Name;
@@ -34,6 +35,40 @@ namespace VendorOrder.Tests
       //Assert
       Assert.AreEqual(name, result);
     }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
+
+      //Act
+      int result = newVendor.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+
+
+    // [TestMethod]
+    // public void GetAll_ReturnsAllVendorObjects_VendorList()
+    // {
+    //   //Arrange
+    //   string name01 = "Utensils";
+    //   string name02 = "Spices";
+    //   Vendor newVendor1 = new Vendor(name01);
+    //   Vendor newVendor2 = new Vendor(name02);
+    //   List<Vendor> newList = new List<Vendor> { newVendor1, newVendor2 };
+
+    //   //Act
+    //   List<Vendor> result = Vendor.GetAll();
+
+    //   //Assert
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+
 
 
   }
